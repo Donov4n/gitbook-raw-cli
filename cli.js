ce5a668551cd47d4375d2c3d4c1b2423e8883f2d
Donovan Lambert <d.lambert@insolus.com>
2017-06-06 15:54:02 +0200

    Remove lodash dependency

--- cli.js
+++ cli.js
@@ -3,27 +3,29 @@
 'use strict';
 
 const gitbook = require('gitbook');
-const _       = require('lodash');
+const omit    = require('lodash.omit');
 const argv    = require('minimist')(process.argv.slice(2));
 
 const command = argv._[0];
-const cmd     = _.find(gitbook.commands, _cmd => (
+const cmd     = gitbook.commands.find(_cmd => (
     _cmd.name.split(' ')[0] === command
 ));
 
 if (!cmd) {
-    throw new Error(`Command ${command} doesn't exist.`);
+    throw new Error(`Command "${command}" doesn't exist.`);
 }
 
-const kwargs = _.omit(argv, '$0', '_');
-_.each(cmd.options || [], (option) => {
-    if (kwargs[option.name] === undefined) {
-        kwargs[option.name] = option.defaults;
-    }
+const kwargs = omit(argv, '$0', '_');
+if (cmd.options) {
+    cmd.options.forEach((option) => {
+        if (kwargs[option.name] === undefined) {
+            kwargs[option.name] = option.defaults;
+        }
 
-    if (option.values && !_.includes(option.values, kwargs[option.name])) {
-        throw new Error(`Invalid value for option "${option.name}"`);
-    }
-});
+        if (option.values && option.values.indexOf(kwargs[option.name]) === -1) {
+            throw new Error(`Invalid value for option "${option.name}"`);
+        }
+    });
+}
 
 cmd.exec(argv._.slice(1), kwargs);
